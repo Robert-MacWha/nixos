034abff9d2082e42cb7f4543d68bd833413eff96
Robert MacWha <github@macwha.com>
2026-06-11 12:34:06 -0400

    update: 2026-06-11T12:34:06-04:00

--- hosts/robert-desktop/home.nix
+++ hosts/robert-desktop/home.nix
@@ -7,8 +7,8 @@
 }:
 let
   # orca-slicer = pkgs.callPackage ../../modules/packages/orca-slicer.nix { inherit unstable; };
-  kurve = pkgs.callPackage ../../modules/packages/kurve.nix { };
-  cavasik = pkgs.callPackage ../../modules/packages/cavasik.nix { };
+  # kurve = pkgs.callPackage ../../modules/packages/kurve.nix { };
+  # cavasik = pkgs.callPackage ../../modules/packages/cavasik.nix { };
   # rotki = pkgs.callPackage ../../modules/packages/rotki.nix { };
 
   rofi-vscode = pkgs.writeShellScriptBin "rofi-vscode" (builtins.readFile ./rofi/vscode.sh);
@@ -42,7 +42,6 @@ in
       mattermost-desktop
       prismlauncher
       jdk17_headless
-      cavasik
       usbutils
       rpi-imager
       ledger-live-desktop

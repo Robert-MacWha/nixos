02eadbf610cd4c927cf6a5eb4902bcb4c2b935ad
Robert MacWha <github@macwha.com>
2026-08-14 19:59:17 -0400

    update: 2026-08-14T19:59:17-04:00

--- modules/services/mainnet.nix
+++ modules/services/mainnet.nix
@@ -11,8 +11,9 @@
     enable = true;
     openFirewall = true;
     settings = {
-      checkpoint-sync-url = "https://mainnet.checkpoint.sigp.io";
+      execution-endpoint = "http://127.0.0.1:8551";
       execution-jwt = config.sops.secrets."jwt_secret".path;
+      checkpoint-sync-url = "https://mainnet.checkpoint.sigp.io";
       http-address = "0.0.0.0";
 
       metrics = true;
